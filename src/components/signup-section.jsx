dbbb6f3b4cda20e9deeacb3a50b6c2800a508307
Robert Kirby <robertaxelkirby@gmail.com>
2018-03-14 17:10:00 +0000

    add  stateless logic for landing page

--- src/components/signup-section.jsx
+++ src/components/signup-section.jsx
@@ -5,15 +5,22 @@ import LoginForm from './login-form';
 import './signup-section.css';
 
 export default function SignupSection() {
-
-	
-
+	let showLogin = false;
+	if (showLogin) {
+		return (
+			<section className="login-signup">
+				<div className="form-wrapper horizontal-center">
+					<LoginForm />
+				</div>
+			</section>
+		)
+	} 
 	return (
 		<section className="login-signup">
 			<SignupText />
 			<div className="form-wrapper horizontal-center">
-				<SignupForm />	
+				<SignupForm />
 			</div>
 		</section>
-	)
+	)	
 }
