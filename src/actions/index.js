fa44639ae6129ce95142ed80d68b8efe62aab255
Robert Kirby <robertaxelkirby@gmail.com>
2018-03-20 15:37:09 +0000

    prep for redux

---
+++ src/actions/index.js
@@ -0,0 +1,5 @@
+export const ADD_PROGRAM = 'ADD_PROGRAM';
+export const addProgram = name => ({
+  type: ADD_PROGRAM,
+  name
+});
---
+++ src/reducers/index.js
@@ -0,0 +1,35 @@
+import * as actions from '../actions';
+
+const initialState = {
+  programs: [{
+    "type": "program",
+    "_id": "5aaf72ca086bbd0004bea321",
+    "name": "Jim's Beach Workout",
+    "userId": "5aaf72ca086bbd0004bea8f0",
+    "summary": "blah blah blah",
+    "dateLastUpdated": "2018-03-16T15:13:09.666Z"
+    }, {
+      "type": "program",
+      "_id": "5aaf72ca086bbd0004bea322",
+      "name": "Jane's Beach Workout",
+      "userId": "5aaf72ca086bbd0004bea8f1",
+      "summary": "blah blah blah",
+      "dateLastUpdated": "2018-03-16T15:13:09.666Z"
+    }
+  ]
+};
+
+export const reducer = (state=initialState, action) => {
+    if (action.type === actions.ADD_PROGRAM) {
+        let name = action.name
+            .replace(/\s+/g, ' ')
+            .trim()
+        return Object.assign({}, state, {
+            programs: [...state.programs, {
+                name: action.name,
+            }]
+        }); 
+    }
+    return state;
+};
+
