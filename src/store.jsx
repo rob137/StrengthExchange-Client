d8a3a59ed1026044aa88c4840346a5e7919af78a
Robert Kirby <robertaxelkirby@gmail.com>
2018-03-28 10:29:26 +0100

    prep for registration auth

--- src/store.jsx
+++ src/store.jsx
@@ -1,10 +1,29 @@
-import {createStore, applyMiddleware} from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import thunk from 'redux-thunk';
+import { loadAuthToken} from './local-storage';
+import authReducer from './reducers/auth';
+import protectedDataReducer from './reducers/protected-data';
+import { setAuthoToken, refreshAuthToken } from './actions/auth';
+import { reducer } from './reducers';
 
-import {reducer} from './reducers';
-
-export default createStore(
-  reducer, 
+const store = createStore(
+  combineReducers({
+    form: formReducer,
+    auth: authReducer,
+    protectedData: protectedDataReducer,
+    reducer,
+  }), 
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(thunk)
 );
+
+// Hydrate the authToken from localStorage
+const authToken = loadAuthToken();
+if (authToken) {
+  const token = authToken;
+  store.dispatch(setAuthToken(token));
+  store.dispatch(refreshAuthToken());
+}
+
+export default store
