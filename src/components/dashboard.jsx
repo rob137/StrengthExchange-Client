521c0ea59154567dcb4576185cb5e696a59d29b2
Robert Kirby <robertaxelkirby@gmail.com>
2018-03-28 17:37:14 +0100

    initial implementation of "requires-login" component

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -3,22 +3,26 @@ import { connect } from 'react-redux';
 
 import ProgramsOverview from './programs-overview';
 import Header from './header';
+import requiresLogin from './requires-login';
 
-import { fetchPrograms } from '../actions';
+import { fetchPrograms } from '../actions/index';
+import { fetchProtectedData } from '../actions/protected-data';
 
 import './dashboard.css';
 
 export class Dashboard extends React.Component {
   componentDidMount() { // Use componentDidRender?  
+    this.props.dispatch(fetchProtectedData());
     this.props.dispatch(fetchPrograms());
   }
 
   render() {
+    const headerText = `${this.props.name}'s Workout Programs`
     return (
       <section className="dashboard">
         <Header
           className="dashboard-header"
-          text="Joe Bloggs' Workout Programs"
+          text={headerText}
         />
         <ProgramsOverview />
       </section>
@@ -26,4 +30,13 @@ export class Dashboard extends React.Component {
   }
 }
 
-export default connect()(Dashboard);
+const mapStateToProps = state => {
+  const { currentUser } = state.auth;
+  return {
+    email: state.auth.currentUser.email,
+    name: `${currentUser.firstName} ${currentUser.lastName}`,
+    protectedData: state.protectedData.data
+  };
+};
+
+export default requiresLogin()(connect(mapStateToProps)(Dashboard));
