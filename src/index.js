8f7bb69218c2120db98737913d66a38ab521c2bf
Robert Kirby <robertaxelkirby@gmail.com>
2018-03-28 10:37:45 +0100

    prep for registration auth

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import store from './store'
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './components/app';
+import store from './store'
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
