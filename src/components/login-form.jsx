6a45df381c44322c291b3d726313dd0b67609a8c
Robert Kirby <robertaxelkirby@gmail.com>
2018-03-28 16:14:31 +0100

    convert to redux form syntax; add "login" from actions/auth

--- src/components/login-form.jsx
+++ src/components/login-form.jsx
@@ -1,17 +1,61 @@
 import React from 'react';
+import { Field, reduxForm, focus } from 'redux-form';
+import Input from './input';
+import { login } from '../actions/auth';
+import { required, nonEmpty } from '../validators';
 
-export default function SignupForm() {
-	return (
-		<section className="login">
-			<h2 className="login-heading">Log In</h2>
-			<form>
-				<label for="email">Email</label>
-				<input id="email" type="text" name="" />
-				<label for="password">Password</label>
-				<input id="password" type="text" name="" />
-				<input type="submit" name="sign up" className="submit-cancel-button" />
-				<button className="submit-cancel-button">Back</button>
-			</form>
-		</section>
-	)
+export class SignupForm extends React.Component {
+	onSubmit(values) {
+		return this.props.dispatch(login(values.email, values.password));
+	}
+	
+	render() {
+		let error;
+		if (this.props.error) {
+			error = (
+				<div className="form-error" aria-live="polite">
+					{this.props.error}
+				</div>
+			);
+		}
+		return (
+			<section className="login">
+				<h2 className="login-heading">Log In</h2>
+				<form
+					className="login-form"
+					onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}
+				>
+					{error}
+					<label htmlFor="email">Email</label>
+					<Field 
+						component={Input} 
+						id="email" 
+						name="email"
+						type="text"
+						validate={[required, nonEmpty]}
+					/>
+					<label htmlFor="password">Password</label>
+					<Field
+						component={Input}
+						id="password" 
+						name="password"
+						type="text"
+						validate={[required, nonEmpty]}
+					/>
+					<button 
+						className="submit-cancel-button" 
+						disabled={this.props.pristine || this.props.submitting}
+					> 
+						Log in
+					</button>
+					<button className="submit-cancel-button">Back</button>
+				</form>
+			</section>
+		);
+	}
 }
+
+export default reduxForm({
+	form: 'login',
+	onSubmitFail: (errors, dispatch) => dispatch(focus('login', 'email'))
+})(SignupForm)
