5b432021af96f700f93e80d1ea9bd8f8380bd42e
Robert Kirby <robertaxelkirby@gmail.com>
2018-03-28 10:12:25 +0100

    alter fields to match back-end

--- src/components/signup-form.jsx
+++ src/components/signup-form.jsx
@@ -2,16 +2,27 @@ import React from 'react';
 
 export default function SignupForm() {
 	return (
-		<form>
-			<label htmlFor="name">Name</label>
-			<input id="name" type="text" name="" />
+		<form 
+			onSubmit={(e) => {
+				e.preventDefault();
+				console.log(1)
+			}}
+		>
+			<label htmlFor="first-name">First Name</label>
+			<input id="first-name" type="text" required name="" />
+			<label htmlFor="first-name">Last Name</label>
+			<input id="first-name" type="text" required name="" />
 			<label htmlFor="email">Email</label>
-			<input id="email" type="text" name="" />
+			<input id="email" type="text" required name="" />
 			<label htmlFor="password">Password</label>
-			<input id="password" type="text" name="" />
+			<input id="password" type="text" required name="" />
 			<label htmlFor="confirm-password">Confirm password</label>
-			<input id="confirm-password" type="text" name="" />
-			<input type="submit" name="sign up" className="submit-cancel-button" />
+			<input id="confirm-password" type="text" required name="" />
+			<input 
+				type="submit" 
+				name="sign up" 
+				className="submit-cancel-button" 
+			/>
 		</form>
 	)
 }
